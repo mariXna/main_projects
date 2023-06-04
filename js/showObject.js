c93e24b83853628820b920c06174344f6ef02e9e
mariXna <chashnytska@gmail.com>
2023-06-04 11:26:57 +0100

    links check added + html construction

--- js/showObject.js
+++ js/showObject.js
@@ -1,3 +1,4 @@
+//04.06.23 - 11:00am
 function showObject(data, thisForm) {
     if (!data) {
         alert('no results!')
@@ -7,107 +8,69 @@ function showObject(data, thisForm) {
     let divContent = $('#content');
     divContent.html('');
 
-    /*for (x in data) {
-        divContent.append('<div>');
-        div = divContent.children(':last').attr('id', data[x].id)
-        div.append('<div>');
-        titleDiv = div.children(':last')
-        titleDiv.append('<h3> Group: <a href="/api/v1/search/?name=' + data[x].title + '" target="search">' + data[x].title + '</a></h3>');
-        if (data[x].abbr > "") {
-            titleDiv.append('<h4> Search as ' + data[x].abbr + '</h4>');
-        }
+    let fillers_list_link = "https://pm-db.net/api/v1/search/fillers/?lang=en&id=${data[x].id_polymers}&counter=0";
+    //let article_link = "https://pm-db.net/article/${data[x].id_article}";
+    let additives_list_link = "https://pm-db.net/api/v1/search/additives/?lang=en&id=${data[x].id_polymers}&counter=0";
 
-        if (data[x].id_article > "") {
-            titleDiv.append('<h4><a href="article/' + data[x].id_article + '" target="search">' + data[x].id_article + '</a></h4>');
-        }
+    var found = false;
+    var html = '';
+    html += `<div>
+  <div class="resultsection">`;
+for (x in data) {
+  var add_flag = data[x].has_additives;
+  console.log(add_flag);
+  var fill_flag = data[x].has_fillers;
+  console.log(add_flag);
+  if (add_flag == true || fill_flag == true) {
+    found = true;
+  }
 
-        for (y in data[x].company_names) {
-            titleDiv.append('<span>' + data[x].company_names[y] + '</span>');
-        }
-
-        for (y in data[x].list) {
-            div.append('<a href="' + data[x].list[y].document_url + '" rel="true" target="_blank">PDF</a>')
-            brand = data[x].list[y].brand
-            if (brand !== undefined) {
-                div.append('<div>' + brand + '<a href="/api/v1/search/analog/' + brand + '" target="search"> Search analog</a></div>')
-            }
-
-            idPolymers = data[x].list[y].id_polymers
-            if (data[x].list[y].has_additives !== false) {
-                div.append('<div><a href="/api/v1/search/additives/?id=' + idPolymers + '" target="search"> Search additives</a></div>')
-            }
-
-            if (data[x].list[y].has_fillers !== false) {
-                div.append('<div><a href="/api/v1/search/fillers/?id=' + idPolymers + '" target="search"> Search fillers</a></div>')
-            }
-
-            div.append('<div>' + data[x].list[y].company_name + '</div>')
-            div.append('<table>');
-            div.append('<thr><thd> Values </thd><thd> Qty </thd> </thr>')
-
-            for (z in data[x].list[y].characteristics) {
-                div.append('<tr><td>' + z + '</td><td></td><td>' + data[x].list[y].characteristics[z] + '</td></tr>');
-            }
-
-            for (z in data[x].list[y].files) {
-                div.append('<p><a href="' + data[x].list[y].files[z].url + '" rel="true" target="_blank">' + data[x].list[y].files[z].title + '</a></p>');
-            }
-
-            notes = data[x].list[y].description
-            if (notes > "") {
-                div.append('<div>' + notes + '</div>')
-            }
-        }
-    }*/
-
-    let article_link = "https://pm-db.net/article/${data[x].id_article}"
-    let additives_list_link = "https://pm-db.net/api/v1/search/additives/?lang=en&id=${data[x].id_polymers}&counter=0"
-
-    for (x in data) {
-        divContent.append(`<div><div class="resultsection">
-          <div class="resultsfields0">
-            <div class="res_field0">
-              <div class="btn0">
-                <span class="all_prod0">
-                  <span>Show all products</span>
-                </span>
-                <div class="expandmore0">
-                </div>
-              </div>`)
-              div.append(`<div class="links">
-              <div class="linkonartcl">
-                  <div class="btn_read_artcl_00">
-                    <div class="icnlink0">
-                    </div>
-                    <span class="read_art0"><span><a href= ${article_link}>Read an article</a></span></span>
-                  </div>
-                </div>
-                <div class="additiveslist0">
-                  <div class="btn_read_artcl_01">
-                    <div class="icnlink1">
-                    </div>
-                    <span class="open_add_list0">
-                      <span><a href=${additives_list_link}>Open additives list</a></span>
-                    </span>
-                  </div>
-                </div>
-                </div><span class="manufacturer0"><span>${data[x].company_names}</span></span>
-              <span class="polymer_group0">
-                <span>${data[x].title}</span>
-              </span>
-            </div>
-          </div>
-          <span class="res_amount">
-            <span>${data.length} polymers were found</span>
-          </span>
-        </div>
-        <div class="searchthroughnavigation">
-          <div class="icnhome">
-          </div>
-          <div class="icnarrow1">
-          </div>
-        </div> 
-        </div>`
-        )
+  html += `<div class="resultsfields0">
+  <div class="res_field0"><div class="btn0"><span class="all_prod0">
+  <span>Show all products</span>
+  </span><div class="expandmore0">
+  </div>
+  </div>`;
+  if (found) {
+    html += '<div class="links">';
+    if (add_flag) {
+      html += `<div class="additiveslist0">
+      <div class="btn_read_artcl_01">
+      <div class="icnlink1">
+      </div><span class="open_add_list0"><span><a href=${additives_list_link}>Open additives list</a></span></span>
+      </div>
+      </div>`;
     }
+    if (fill_flag){
+      html += `<div class="linkonartcl">
+      <div class="btn_read_artcl_00">
+        <div class="icnlink0">
+        </div>
+        <span class="read_art0"><span><a href= ${fillers_list_link}>Open fillers list</a></span></span>
+      </div>
+    </div>`;
+    }
+    html += '</div>' 
+  } 
+  html += `<span class="manufacturer0"><span>${data[x].company_names}</span></span>
+  <span class="polymer_group0">
+    <span>${data[x].title}</span>
+  </span>
+  </div>
+  </div>`;
+  
+  
+
 }
+
+html += `<span class="res_amount">
+<span>${data.length} polymer(s) were found</span>
+</span>
+</div>
+<div class="searchthroughnavigation">
+<div class="icnhome">
+</div>
+<div class="icnarrow1"></div></div></div>`;
+var content = document.getElementById('content');
+content.innerHTML = html;
+}   
