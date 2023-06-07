855bfdd10b1ed5c8cc166803dfe4670398b40780
mariXna <chashnytska@gmail.com>
2023-06-07 22:38:13 +0100

    button all prod changed

--- js/showObject.js
+++ js/showObject.js
@@ -57,12 +57,12 @@ for (x in data) {
   }
 
   html += `<div class="resultsfields0">
-  <button class="button_all_prod" onclick="openHiddenSection()"><div class="btn0">
+  <button class="btn0" onclick="openHiddenSection()>
   <span class="all_prod0">
   <span>Show ${data[x].list.length} product(s)</span>
   </span>
   <div class="expandmore0"></div>
-</div></button>
+</button>
   </div>`;
 
   for (y in data[x].list){
