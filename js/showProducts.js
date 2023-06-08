faf6b848fb1ca39e216255aeb85f03239875ea48
mariXna <chashnytska@gmail.com>
2023-06-08 05:23:51 +0100

    updated date and html

--- js/showProducts.js
+++ js/showProducts.js
@@ -1,161 +1,163 @@
-//05.06.23 - 15.50am BST
-/*for (y in data[x].list){
-  html_prod += `<div class="container">
-  <div class="resultsection_prod">
-    <div class="resultfield_prod">
-      <div class="content_prod">
-        <img
-          src="public/external/divider1369-s7tk.svg"
-          alt="divider1369"
-          class="divider_top_prod"
-        />
-        <div class="linksondocs_prod">
-          <div class="link_tds_prod">
-            <span class="text_tds_prod">
-              <span>Read TDS here</span>
-            </span>
-            <div class="icndoc_prod">
-              <img
-                src="public/external/bound1369-gsha-200h.png"
-                alt="bound1369"
-                class="bound_prod"
-              />
-              <img
-                src="public/external/vector1369-9459.svg"
-                alt="Vector1369"
-                class="vector0_prod"
-              />
-              <img
-                src="public/external/vector11369-3h1.svg"
-                alt="Vector11369"
-                class="vector1_prod"
-              />
-              <img
-                src="public/external/vector21369-nsy9.svg"
-                alt="Vector21369"
-                class="vector2_prod"
-              />
-              <img
-                src="public/external/vector31369-zufu.svg"
-                alt="Vector31369"
-                class="vector3_prod"
-              />
-            </div>
-          </div>
-          <div class="link_msds_prod">
-            <span class="text_msds_prod">
-              <span>Read MSDS here</span>
-            </span>
-            <div class="icndoc01_prod">
-              <img
-                src="public/external/bound1369-wops-200h.png"
-                alt="bound1369"
-                class="bound01_prod"
-              />
-              <img
-                src="public/external/vector1369-rhzl.svg"
-                alt="Vector1369"
-                class="vector00_prod"
-              />
-              <img
-                src="public/external/vector11369-aqf2.svg"
-                alt="Vector11369"
-                class="vector01_prod"
-              />
-              <img
-                src="public/external/vector21369-skp.svg"
-                alt="Vector21369"
-                class="vector02_prod"
-              />
-              <img
-                src="public/external/vector31369-5syq.svg"
-                alt="Vector31369"
-                class="vector03_prod"
-              />
-            </div>
-          </div>
-          <div class="link_presentation_prod">
-            <span class="text_presentation_prod">
-              <span>Presentation</span>
-            </span>
-            <div class="icnslides_prod">
-              <img
-                src="public/external/vector1369-t5enf.svg"
-                alt="Vector1369"
-                class="vector12_prod"
-              />
-              <img
-                src="public/external/vector1369-ofoh.svg"
-                alt="Vector1369"
-                class="vector13_prod"
-              />
-            </div>
-          </div>
-        </div>
-        <div class="downloadbtn_prod">
-          <span class="text_downloadpdf_prod">
-            <span>Download .pdf</span>
-          </span>
-        </div>
-        <div class="mechchardropdown_prod">
-          <span class="text_mechchar_prod">
-            <span>Mechanical characteristics</span>
-          </span>
-          <div class="expandmore_prod">
-            <img
-              src="public/external/shape1369-41xh.svg"
-              alt="Shape1369"
-              class="shape_prod"
-            />
-          </div>
-        </div>
-        <span class="text_prod">
-          <span>
-            Продукт являє собою препег, що армований скляним волокном та
-            наповнений мінеральними дисперсними наповнювачами. Під дією
-            відкритого полум’я не плавиться, не спричиняє падіння крапель
-            та не утворює великої кількості диму.
-          </span>
-        </span>
-      </div>
-      <span class="text_name_prod"><span>${italic_bold(data[x].list[y].title)}</span></span>
-      <div class="gapsolution_prod">
-        <span class="text_analogs_prod">
-          <span>Analogs are available</span>
-        </span>
-        <span class="text_open_list_prod">
-          <span>open the list</span>
-        </span>
-        <div class="icnavalbl_prod">
-          <div class="vectr_prod">
-            <img
-              src="public/external/vector1369-twih.svg"
-              alt="Vector1369"
-              class="vector14_prod"
-            />
-            <img
-              src="public/external/vector1369-fgdf.svg"
-              alt="Vector1369"
-              class="vector15_prod"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-      </div>
-    </div>
-    <img
-      src="public/external/divider1369-l3qu.svg"
-      alt="divider1369"
-      class="divider_side_prod"
-    />
-    <span class="res_amount_prod">
-      <span>
-        5 products of unsaturated resin compounds are available now
-      </span>
-    </span>
+//08.06.23 - 5am BST
+/*html_prod += `<div id="hiddenContent${x}" style="display:none" ><div class="container">
+    <div class="resultsection_prod">`;
+
     
-  </div>
-</div>
-</div>`;
-}*/
+
+    for (var y in data[x].list){
+
+    tds_link = data[x].list[y].files[0].url;
+    msds_link = data[x].list[y].files[1].url;
+    presentation_link = data[x].list[y].files[2].url;
+
+      html_prod += `
+        <div class="resultfield_prod">
+          <div class="content_prod">
+            <img
+              src="public/external/divider1369-s7tk.svg"
+              alt="divider1369"
+              class="divider_top_prod"
+            />
+            <div class="linksondocs_prod">
+              <div class="link_tds_prod">
+                <span class="text_tds_prod">
+                  <span><a href=${tds_link}>Read TDS here</a></span>
+                </span>
+                <div class="icndoc_prod">
+                  <img
+                    src="public/external/bound1369-gsha-200h.png"
+                    alt="bound1369"
+                    class="bound_prod"
+                  />
+                  <img
+                    src="public/external/vector1369-9459.svg"
+                    alt="Vector1369"
+                    class="vector0_prod"
+                  />
+                  <img
+                    src="public/external/vector11369-3h1.svg"
+                    alt="Vector11369"
+                    class="vector1_prod"
+                  />
+                  <img
+                    src="public/external/vector21369-nsy9.svg"
+                    alt="Vector21369"
+                    class="vector2_prod"
+                  />
+                  <img
+                    src="public/external/vector31369-zufu.svg"
+                    alt="Vector31369"
+                    class="vector3_prod"
+                  />
+                </div>
+              </div>
+              <div class="link_msds_prod">
+                <span class="text_msds_prod">
+                  <span><a href=${msds_link}>Read MSDS here</a></span>
+                </span>
+                <div class="icndoc01_prod">
+                  <img
+                    src="public/external/bound1369-wops-200h.png"
+                    alt="bound1369"
+                    class="bound01_prod"
+                  />
+                  <img
+                    src="public/external/vector1369-rhzl.svg"
+                    alt="Vector1369"
+                    class="vector00_prod"
+                  />
+                  <img
+                    src="public/external/vector11369-aqf2.svg"
+                    alt="Vector11369"
+                    class="vector01_prod"
+                  />
+                  <img
+                    src="public/external/vector21369-skp.svg"
+                    alt="Vector21369"
+                    class="vector02_prod"
+                  />
+                  <img
+                    src="public/external/vector31369-5syq.svg"
+                    alt="Vector31369"
+                    class="vector03_prod"
+                  />
+                </div>
+              </div>
+              <div class="link_presentation_prod">
+                <span class="text_presentation_prod">
+                  <span><a href=${presentation_link}>Presentation</a></span>
+                </span>
+                <div class="icnslides_prod">
+                  <img
+                    src="public/external/vector1369-t5enf.svg"
+                    alt="Vector1369"
+                    class="vector12_prod"
+                  />
+                  <img
+                    src="public/external/vector1369-ofoh.svg"
+                    alt="Vector1369"
+                    class="vector13_prod"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="downloadbtn_prod">
+              <span class="text_downloadpdf_prod">
+                <span>Download .pdf</span>
+              </span>
+            </div>
+            <div class="mechchardropdown_prod">
+              <span class="text_mechchar_prod">
+                <span>Mechanical characteristics</span>
+              </span>
+              <div class="expandmore_prod">
+                <img
+                  src="public/external/shape1369-41xh.svg"
+                  alt="Shape1369"
+                  class="shape_prod"
+                />
+              </div>
+            </div>
+            <span class="text_prod">
+              <span>
+                ${data[x].list[y].description}
+              </span>
+            </span>
+          </div>
+          <span class="text_name_prod"><span>${italic_bold(data[x].list[y].title)}</span></span>
+          <div class="gapsolution_prod">
+            <span class="text_analogs_prod">
+              <span>Analogs are available</span>
+            </span>
+            <span class="text_open_list_prod">
+              <span>open the list</span>
+            </span>
+            <div class="icnavalbl_prod">
+              <div class="vectr_prod">
+                <img
+                  src="public/external/vector1369-twih.svg"
+                  alt="Vector1369"
+                  class="vector14_prod"
+                />
+                <img
+                  src="public/external/vector1369-fgdf.svg"
+                  alt="Vector1369"
+                  class="vector15_prod"
+                />
+              </div>
+            </div>
+          </div>
+        
+        </div>`;
+    }
+
+    html_prod += `<img
+    src="public/external/divider1369-l3qu.svg"
+    alt="divider1369"
+    class="divider_side_prod"
+  />
+  <span class="res_amount_prod">
+    <span>
+      ${data[x].list.length} products of ${italic_bold(data[x].title)} are available now
+    </span>
+  </span></div></div></div></div>`;*/
