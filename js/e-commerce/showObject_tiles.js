8eb21e50993fb47f72c6781866a8b4efc924a386
mariXna <chashnytska@gmail.com>
2023-10-05 16:22:17 +0100

    jquery filter forms added

--- js/e-commerce/showObject_tiles.js
+++ js/e-commerce/showObject_tiles.js
@@ -23,7 +23,7 @@ function addEllipsis() {
         content.innerHTML = html;
           return;
       }
-
+    $('.sidebar-section').load('show/forms/filters')
     var html = '';
     html+=`
     <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic&display=swap&subset=cyrillic-ext" rel="stylesheet" /><div class="search_tiles">`;
