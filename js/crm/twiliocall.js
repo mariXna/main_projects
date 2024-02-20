8075fd73c0da565a8a7b2b98c9be08b98f85570c
mariXna <chashnytska@gmail.com>
2024-02-20 13:48:30 +0000

    deleted twilio_call

--- js/crm/twiliocall.js
+++ js/crm/twiliocall.js
@@ -1,7 +1,9 @@
+ var device;
+
 function device_call(form){
-  var incoming_num = form.calling_to.value;
-  var from_num = form.calling_from.value;
-  console.log(from_num);
+
+  var calling_to = form.calling_to.value;
+  var calling_from = form.calling_from.value;
 
   const token_link = '/twillio/token';
   async function fetchToken() {
@@ -14,6 +16,7 @@ function device_call(form){
         return token;
     } catch (error) {
         console.error('Error fetching token:', error);
+        fetchToken();
     }
 }
 
@@ -22,12 +25,11 @@ function device_call(form){
   fetchToken().then(token => {
     if (!token) {
       fancyOpen("<span>'No token received'</span>")
-      console.error('No token received');
+
       return;
     }
 
-    
-    const device = new Twilio.Device(token, {
+    device = new Twilio.Device(token, {
       appName: "TEST APP",
       appVersion: "0.0.1",
       closeProtection: true,
@@ -38,46 +40,26 @@ function device_call(form){
 
     device.on('ready', function (device) {
       fancyOpen("<span>'Twilio.Device is ready to make calls!'</span>")
-      console.log('Twilio.Device is ready to make calls!');
     });
 
     device.on('error', function (error) {
       fancyOpen(`<span>'Twilio.Device Error: ${error.message}</span>`)
-      console.error('Twilio.Device Error: ' + error.message);
     });
 
     const params = {
-      To: incoming_num,
-      From: from_num,
+      To: calling_to,
+      From: calling_from,
       agent: "Smith"
     };
 
-    console.log(`Calling ${params.To} from ${params.From}...`);
-    
-    //device.connect({params:params});
-
-
-
+    device.connect({params:params});
 });
 
 return false;
 } 
 
-function hang_up_call(){
+function hang_up_call(device){
   device.on('disconnect', function (call) {
     fancyOpen(`<span>'Call ended'</span>`)
-    console.log('Call ended');
   });
 }
-function twilio_call(incoming_num, from_num) {
-
-  html_form = `<form name="call_form" action="" method="GET"><label for="calling_to">Calling:</label>
-      <label for="calling_to">${incoming_num}</label><br>
-      <label for="calling_from">from:</label>
-      <input type="text" id="calling_from" name="calling_from" value=${from_num}><br><br>
-      <button type="button" onclick="device_call(${incoming_num}, this.form)">Call</button></form>`;
-
-  fancyOpen(html_form);
-  
-    
-};
